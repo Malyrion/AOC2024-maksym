a4c30cd78225a211df45d6b249df39ac8f03215a
Malyrion <maksym.1002@gmail.com>
2024-12-01 19:27:07 +0000

    day 1 of AOC

---
+++ Day1/part1.py
@@ -0,0 +1,62 @@
+# create two lists reading form the txt file
+# order the list in to ascending order
+# loop through and calculate the distance inot a sum variable
+
+def make_two_arrays(file_name)->[[int],[int]]:
+    """
+    Read the txt file in specific format and returns two arrays
+
+    Args:
+         file_name (txt): a txt file with two list of numbers saperated by "   " and "/n" on the second number in line
+
+    Returns:
+        array1: the sum dstances between all elements in two arrays
+        array2:
+    """
+    array1:[int]=[]
+    array2:[int]=[]
+
+    file = open(file_name)
+    for line in file:
+        pointer=line.split("   ")
+        array1.append(int(pointer[0]))
+        array2.append(int(pointer[1][:-1]))
+
+    print(array1)
+    print(array2)
+
+    return array1, array2
+
+
+
+
+def sum_distance(array1:[int], array2:[int])->int:
+    """
+    Sum the difference between values in two arrays
+
+    Args:
+         array1 ([nums]): first array of numbers
+         array2 ([nums]): second array of numbers
+
+    Returns:
+        int: the sum of distances between all elements in two arrays
+    """
+
+    #sort both arrays into ascending order
+    array1.sort()
+    array2.sort()
+    sum=0
+    print(array1)
+    print(array2)
+
+    for i in range(0,len(array1)):
+        if(array1[i]>array2[i]):
+            print(array1[i]-array2[i])
+            sum+=array1[i]-array2[i]
+        else:
+            print(array2[i]-array1[i])
+            sum+=array2[i]-array1[i]
+    return sum
+
+array1, array2 = make_two_arrays("input.txt")
+print(sum_distance(array1,array2))
---
+++ Day1/part2.py
@@ -0,0 +1,61 @@
+from typing import List, Dict
+
+def make_two_arrays(file_name)->[[int],[int]]:
+    """
+    Read the txt file in specific format and returns two arrays
+
+    Args:
+         file_name (txt): a txt file with two list of numbers saperated by "   " and "/n" on the second number in line
+
+    Returns:
+        array1: first array of int
+        array2: second array of int
+    """
+    array1:[int]=[]
+    array2:[int]=[]
+
+    file = open(file_name)
+    for line in file:
+        pointer=line.split("   ")
+        array1.append(int(pointer[0]))
+        array2.append(int(pointer[1][:-1]))
+
+    print(array1)
+    print(array2)
+
+    return array1, array2
+
+
+"""
+    Run through second array make a dictionary of howmuch would you multiply 
+    than run and multiply by indes of the dictionary
+"""
+
+def find_similarity_score(array1:[int],array2:[int])->int:
+
+    """
+    Run throught array2 and make a dictionary of how many times each number is appearing
+    than multiple each element form array1 based on count in the dictionary via the same key
+
+    Args:
+         array1: first array of integers
+         array2: second array of integers
+
+    Returns:
+        simularity_score: the simularity_score between all elements in two arrays
+
+    """
+    repetition: Dict[int,int]={}
+    for number in array2:
+        repetition[number]=array2.count(number)
+    print(repetition)
+    simularity_score:int = 0
+    for number in array1:
+        try:
+            simularity_score+=number*repetition[number]
+        except:
+            pass
+    return simularity_score
+
+array1,array2 = make_two_arrays('input.txt')
+print(find_similarity_score(array1,array2))
